3007b18ae4e4b91def6a3fc60b9eebf7660f5032
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-07-11 10:08:33 +0200

    reuse comma separated ranges implementation

--- lib/makita/filter/rational.rb
+++ lib/makita/filter/rational.rb
@@ -1,52 +1,10 @@
 require_relative "base"
+require_relative "comma_separated_ranges"
 
 module Makita
   module Filter
     class Rational < Base
-      def conditions
-        terms = filter_value.split(",")
-        ranges, simples = terms.partition {|term| term[range_separator_re] }
-        term_conditions = [simple_condition(simples)] + range_conditions(ranges)
-        or_conditions(*term_conditions)
-      end
-
-      private
-
-      def range_separator_re
-        Regexp.union(sep_to_op_mapping.keys)
-      end
-
-      def sep_to_op_mapping
-        {"~" => "<"}
-      end
-
-      def range_sep_to_op sep
-        sep_to_op_mapping.fetch(sep)
-      end
-
-      def simple_condition values
-        case values.count
-        when 0
-          nil
-        when 1
-          ["#{name} = ?", values.first]
-        else
-          ["#{name} in (?)", values]
-        end
-      end
-
-      def range_conditions ranges
-        ranges.map{|range| range_condition(range)}
-      end
-
-      def range_condition range
-        left, right = range.split(range_separator_re)
-        separator = range[range_separator_re]
-        right_op = range_sep_to_op(separator)
-        left_condition = ["(#{name} >= ?)", left] if left.present?
-        right_condition = ["(#{name} #{right_op} ?)", right] if right.present?
-        and_conditions(left_condition, right_condition)
-      end
+      include CommaSeparatedRanges
     end
   end
 end
